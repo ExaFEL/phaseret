ca77761be7ddc6ee1248eb65a030e2e323a1698b
Antoine Dujardin <dujardin@slac.stanford.edu>
2019-06-06 14:19:46 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='phaseret',
+      version='0.0.0',
+      description='XFEL phase retrieval package',
+      url='http://github.com/storborg/funniest',
+      author='Antoine Dujardin',
+      author_email='dujardin@slac.stanford.edu',
+      packages=['phaseret'])
