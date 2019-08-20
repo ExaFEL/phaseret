a8194be7b65cb3723cd8f2a0189bd2de88817848
Antoine Dujardin <dujardin@slac.stanford.edu>
2019-08-20 16:20:39 -0700

    Correct url in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='phaseret',
       version='0.0.0',
       description='XFEL phase retrieval package',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/ExaFEL/phaseret',
       author='Antoine Dujardin',
       author_email='dujardin@slac.stanford.edu',
       packages=['phaseret'])
